238896bfd8ef13c5588b35f2219e16e192eadfd1
MariaFernanda <mafaduartemelot216@gmail.com>
2025-06-27 10:39:19 -0300

    refactor: alteração da solicitação de valores para o usuário

--- src/IPhone.java
+++ src/IPhone.java
@@ -1,5 +1,7 @@
-public class IPhone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+import java.util.Scanner;
 
+public class IPhone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+    
     // Implementação dos métodos do Reprodutor Musical
     public void tocar() {
         System.out.println("Reproduzindo música...");
@@ -42,20 +44,93 @@ public class IPhone implements ReprodutorMusical, AparelhoTelefonico, NavegadorI
     // Método main para testes
     public static void main(String[] args) {
         IPhone iphone = new IPhone();
+        Scanner scanner = new Scanner(System.in);
 
-        // Testando Reprodutor Musical
-        iphone.selecionarMusica("Imagine - John Lennon");
-        iphone.tocar();
-        iphone.pausar();
+        System.out.println("O que você deseja fazer no IPhone?");
+        System.out.println("1 - Reproduzir música");
+        System.out.println("2 - Fazer uma ligação");
+        System.out.println("3 - Navegar na internet");
+        System.out.print("Escolha uma opção: ");
+        int opcao = scanner.nextInt();
+        scanner.nextLine(); // Limpa o buffer
+        while (opcao != 0) {
+            switch (opcao) {
+            case 1:
+                System.out.print("Informe o nome da música: ");
+                String musica = scanner.nextLine();
+                iphone.selecionarMusica(musica);
 
-        // Testando Aparelho Telefônico
-        iphone.ligar("11999999999");
-        iphone.atender();
-        iphone.iniciarCorreioVoz();
+                System.out.println("O que deseja fazer com a música?");
+                System.out.println("1 - Tocar");
+                System.out.println("2 - Pausar");
+                int opMusica = scanner.nextInt();
+                scanner.nextLine(); // Limpa o buffer
 
-        // Testando Navegador na Internet
-        iphone.exibirPagina("https://www.exemplo.com");
-        iphone.adicionarNovaAba();
-        iphone.atualizarPagina();
+                switch (opMusica) {
+                    case 1:
+                        iphone.tocar();
+                        break;
+                    case 2:
+                        iphone.pausar();
+                        break;
+                    default:
+                        System.out.println("Opção inválida.");
+                }
+                break;
+            case 2:
+                System.out.println("O que deseja fazer com o telefone?");
+                System.out.println("1 - Ligar");
+                System.out.println("2 - Atender");
+                System.out.println("3 - Acessar correio de voz");
+                System.out.print("Escolha uma opção: ");
+                int opTelefone = scanner.nextInt();
+                scanner.nextLine(); // Limpa o buffer
+
+                switch (opTelefone) {
+                    case 1:
+                        System.out.print("Informe o número para ligar: ");
+                        String numero = scanner.nextLine();
+                        iphone.ligar(numero);
+                        break;
+                    case 2:
+                        iphone.atender();
+                        break;
+                    case 3:
+                        iphone.iniciarCorreioVoz();
+                        break;
+                    default:
+                        System.out.println("Opção inválida.");
+                }
+                break;
+            case 3:
+                System.out.println("O que deseja fazer no navegador?");
+                System.out.println("1 - Exibir página");
+                System.out.println("2 - Adicionar nova aba");
+                System.out.println("3 - Atualizar página");
+                System.out.print("Escolha uma opção: ");
+                int opNavegador = scanner.nextInt();
+                scanner.nextLine(); // Limpa o buffer
+
+                switch (opNavegador) {
+                    case 1:
+                        System.out.print("Informe a URL: ");
+                        String url = scanner.nextLine();
+                        iphone.exibirPagina(url);
+                        break;
+                    case 2:
+                        iphone.adicionarNovaAba();
+                        break;
+                    case 3:
+                        iphone.atualizarPagina();
+                        break;
+                    default:
+                        System.out.println("Opção inválida.");
+                }
+                break;
+            default:
+                System.out.println("Opção inválida.");
+            }
+        }
+        
     }
 }
